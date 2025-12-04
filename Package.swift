69680f085ae792968f76b1df184c61ab022bf7de
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-12-03 20:15:20 -0800

    package.swift

--- Package.swift
+++ Package.swift
@@ -3,146 +3,33 @@
 
 import PackageDescription
 
-let name: String = "SPFKTesting" // Swift target
-var localDependencies: [RemoteDependency] = []
-let remoteDependencies: [RemoteDependency] = []
-let resources: [PackageDescription.Resource]? = [.process("Resources")]
-
-let nameC: String? = nil
-let dependencyNamesC: [String] = []
-let remoteDependenciesC: [RemoteDependency] = []
-var cSettings: [PackageDescription.CSetting]? { [
-    .headerSearchPath("include_private")
-] }
-var cxxSettings: [PackageDescription.CXXSetting]? { [
-    .headerSearchPath("include_private")
-] }
-let platforms: [PackageDescription.SupportedPlatform]? = [
-    .macOS(.v12)
-]
-
-// MARK: - Reusable Code for a dual Swift + C package --------------------------------------------------
-
-let githubBase = "https://github.com/ryanfrancesconi"
-
-struct RemoteDependency {
-    let package: PackageDescription.Package.Dependency
-    let product: PackageDescription.Target.Dependency
-}
-
-var localDependencyNames: [String] {
-    localDependencies.compactMap {
-        switch $0.product {
-        case let .productItem(name: productName, package: _, moduleAliases: _, condition: _):
-            productName
-        default:
-            nil
-        }
-    }
-}
-
-var swiftTarget: PackageDescription.Target {
-    var targetDependencies: [PackageDescription.Target.Dependency] {
-        let names = localDependencyNames.filter { $0 != "SPFKTesting" }
-
-        var value: [PackageDescription.Target.Dependency] = names.map {
-            .byNameItem(name: "\($0)", condition: nil)
-        }
-
-        if let nameC {
-            value.append(.target(name: nameC))
-        }
-
-        value.append(contentsOf: remoteDependencies.map(\.product))
-
-        return value
-    }
-
-    return .target(
-        name: name,
-        dependencies: targetDependencies,
-        resources: resources
-    )
-}
-
-var testTarget: PackageDescription.Target {
-    var targetDependencies: [PackageDescription.Target.Dependency] {
-        var array: [PackageDescription.Target.Dependency] = [
-            .byNameItem(name: name, condition: nil)
-        ]
-
-        if let nameC {
-            array.append(.byNameItem(name: nameC, condition: nil))
-        }
-
-        if localDependencyNames.contains("SPFKTesting") {
-            array.append(.byNameItem(name: "SPFKTesting", condition: nil))
-        }
-
-        return array
-    }
-
-    let nameTests: String = "\(name)Tests" // Test target
-
-    return .testTarget(
-        name: nameTests,
-        dependencies: targetDependencies,
-        resources: nil,
-        swiftSettings: [
-            .swiftLanguageMode(.v5),
-            .unsafeFlags(["-strict-concurrency=complete"]),
-        ],
-    )
-}
-
-var cTarget: PackageDescription.Target? {
-    guard let nameC else { return nil }
-
-    var targetDependencies: [PackageDescription.Target.Dependency] {
-        var value: [PackageDescription.Target.Dependency] = dependencyNamesC.map {
-            .byNameItem(name: "\($0)", condition: nil)
-        }
-
-        value.append(contentsOf: remoteDependenciesC.map(\.product))
-
-        return value
-    }
-
-    return .target(
-        name: nameC,
-        dependencies: targetDependencies,
-        publicHeadersPath: "include",
-        cSettings: cSettings,
-        cxxSettings: cxxSettings
-    )
-}
-
-var targets: [PackageDescription.Target] {
-    [swiftTarget, cTarget, testTarget].compactMap(\.self)
-}
-
-var packageDependencies: [PackageDescription.Package.Dependency] {
-    localDependencies.map(\.package) +
-        remoteDependencies.map(\.package) +
-        remoteDependenciesC.map(\.package)
-}
-
-var products: [PackageDescription.Product] {
-    let targets: [String] = [name, nameC].compactMap(\.self)
-
-    return [
-        .library(name: name, targets: targets)
-    ]
-}
-
-// This is required to be at the bottom
-
 let package = Package(
-    name: name,
-    defaultLocalization: "en",
-    platforms: platforms,
-    products: products,
-    dependencies: packageDependencies,
-    targets: targets,
-    cxxLanguageStandard: .cxx20
+    name: "SPFKTesting",
+    platforms: [
+        .macOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "SPFKTesting",
+            targets: [
+                "SPFKTesting",
+            ]
+        ),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "SPFKTesting",
+            dependencies: [
+            ]
+        ),
+
+        .testTarget(
+            name: "SPFKTestingTests",
+            dependencies: [
+                "SPFKTesting",
+            ]
+        ),
+    ]
 )
