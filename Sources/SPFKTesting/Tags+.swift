c292eb215b748e28666f1d12378d16c9ea284fd1
Ryan Francesconi <thiscomputermagic@gmail.com>
2025-12-07 08:56:28 -0800

    refactor cleanup

--- Sources/SPFKTesting/Tags+.swift
+++ Sources/SPFKTesting/Tags+.swift
@@ -4,4 +4,7 @@ extension Tag {
     /// A tag to indicate this is a development test rather than functionality that should be unit tested
     @Tag public static var development: Self
     @Tag public static var file: Self
+    @Tag public static var automation: Self
+    @Tag public static var realtime: Self
+    @Tag public static var engine: Self
 }
